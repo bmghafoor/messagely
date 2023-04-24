3b098002fa53fcaca686bdcbcbf9d4f618851a42
Binyameen <bmghafoor@gmail.com>
2023-04-23 23:39:43 -0400

    Added functionality for routes

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,53 @@
-/** GET / - get list of users.
- *
- * => {users: [{username, first_name, last_name, phone}, ...]}
- *
- **/
+const Router = require("express").Router;
+const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const ExpressError = require("../expressError");
 
+const router = new Router();
 
-/** GET /:username - get detail of users.
- *
- * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
- *
- **/
+router.get("/", ensureLoggedIn, async (req, res, next) => {
+  try {
+    let users = await User.all();
+    return res.json({ users });
+  } catch (error) {
+    return next(error);
+  }
+});
 
+router.get("/:username", ensureLoggedIn, async (req, res, next) => {
+  try {
+    let user = await User.get(req.params.username);
+    if (!user) {
+      throw new ExpressError(`User ${req.params.username} does not exist`, 404);
+    }
+    return res.json({ user });
+  } catch (error) {
+    return next(error);
+  }
+});
 
-/** GET /:username/to - get messages to user
- *
- * => {messages: [{id,
- *                 body,
- *                 sent_at,
- *                 read_at,
- *                 from_user: {username, first_name, last_name, phone}}, ...]}
- *
- **/
+router.get("/:username/to", ensureCorrectUser, async (req, res, next) => {
+  try {
+    let user = await User.get(req.params.username);
+    if (!user) {
+      throw new ExpressError(`User ${req.params.username} does not exist`, 404);
+    }
+    let messages = await User.messagesTo(req.params.username);
+    return res.json({ messages });
+  } catch (error) {
+    return next(error);
+  }
+});
 
-
-/** GET /:username/from - get messages from user
- *
- * => {messages: [{id,
- *                 body,
- *                 sent_at,
- *                 read_at,
- *                 to_user: {username, first_name, last_name, phone}}, ...]}
- *
- **/
+router.get("/:username/from", ensureCorrectUser, async (req, res, next) => {
+  try {
+    let user = await User.get(req.params.username);
+    if (!user) {
+      throw new ExpressError(`User ${req.params.username} does not exist`, 404);
+    }
+    let messages = await User.messagesFrom(req.params.username);
+    return res.json({ messages });
+  } catch (error) {
+    return next(error);
+  }
+});
